5fca7c637e02ff2edbdcd6b9ef3b24ca3e336055
Cian <cian.omondi@student.moringaschool.com>
2021-09-09 23:53:52 +0300

    added the path to view functions

--- gram/urls.py
+++ gram/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main,name = 'home')
-
+    path('home/', views.main,name = 'home'),
+    path('register/',views.register, name='register'),
+    path('',views.loginuser,name='login_user')
 ]
