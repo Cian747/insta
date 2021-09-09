284244c1b2f44da4e2c82e90adc6316633c78cfc
Cian <cian.omondi@student.moringaschool.com>
2021-09-09 23:53:31 +0300

    created view for registration and login

--- gram/views.py
+++ gram/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import RegistrationForm
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate,login,logout
 
 # Create your views here.
+def register(request):
+    rgf = RegistrationForm()
+    if request.method == 'POST':
+        rgf = RegistrationForm(request.POST)
+        if rgf.is_valid():
+            rgf.save()
+            user = rgf.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('login_user')
+    
+    return render(request, 'registration/registration_form.html', {'rgf': rgf})
+
+def loginuser(request):
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username = username, password = password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+
+    return render(request, 'registration/login.html')
+
 
 def main(request):
-    return render(request,'base.html')
+    return render(request,'index.html')
