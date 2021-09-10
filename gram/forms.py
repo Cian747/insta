6ad3de4c347a2c36a8c2fe75c4a0f2653b54288a
Cian <cian.omondi@student.moringaschool.com>
2021-09-10 08:39:24 +0300

    Registration form update

--- gram/forms.py
+++ gram/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
-class RegistrationForm(UserCreationForm):
+class InstaRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email', 'username','password1','password2' ]
