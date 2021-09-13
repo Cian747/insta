2d763833ae319a4d3d4019fb98bf0c57fb24e812
Cian <cian.omondi@student.moringaschool.com>
2021-09-13 22:55:55 +0300

    Created forms

--- gram/forms.py
+++ gram/forms.py
@@ -1,6 +1,9 @@
+from django.forms import widgets
+from gram.models import Image
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Image,Comment,Profile
 
 
 class InstaRegistrationForm(UserCreationForm):
@@ -15,4 +18,34 @@ class InstaRegistrationForm(UserCreationForm):
             'username':forms.TextInput(attrs = {'class':'form-control names', 'placeholder':"Username", 'label': 'Username'}),
             'password1':forms.PasswordInput(attrs = {'class':'form-control ','type':'password', 'placeholder':"Password", 'label': 'Password'}),
             'password2':forms.PasswordInput(attrs = {'class':'form-control', 'placeholder':"Confirm Password", 'label': 'Confirm Password'}),
+        }
+
+class PostImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = ['image','image_name','image_caption','profile']
+
+        widgets = {
+            'image':forms.FileInput(attrs={'class':'form-control'}),
+            'image_name':forms.TextInput(attrs={'class':'form-control names','placeholder':'In a word...','label':'Put a name'}),
+            'image_caption':forms.Textarea(attrs={'class':'form-control names','placeholder':"Caption",'label':"Caption"}),
+        }
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['comment']
+
+        widgets = {
+            'comment': forms.TextInput(attrs={'class':'form-control','name':'comment'})
+        }
+
+class EditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['profile_photo','bio']
+
+        widgets = {
+            'profile_photo':forms.FileInput(attrs={'class':'form-control'}),
+            'bio':forms.Textarea(attrs={'class':'form-control names','placeholder':'Write here...','label':'Put a name'}),
         }
