f832136361c31544af0cc43fd12940f9f46f46dc
Cian <cian.omondi@student.moringaschool.com>
2021-09-13 22:58:47 +0300

    added methods to profile models

--- profiles/models.py
+++ profiles/models.py
@@ -9,12 +9,18 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     following = models.ManyToManyField(User,related_name='following', blank=True)
     bio = models.TextField(default="Hi I'm new here")
-    profile_photo = models.ImageField(upload_to='images/',default= 'https://avataaars.io/?avatarStyle=Circle&topType=WinterHat3&accessoriesType=Round&hatColor=Gray02&facialHairType=MoustacheMagnum&facialHairColor=Red&clotheType=BlazerShirt&eyeType=Wink&eyebrowType=Angry&mouthType=Twinkle&skinColor=Black')
+    profile_photo = models.ImageField(upload_to='images/',blank=True,null=True)
     updated = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def image_posts(self):
         return self.image_set.all()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
 
     def __str__(self):
         return str(self.user.username)
@@ -28,3 +34,8 @@ class Profile(models.Model):
     @classmethod
     def search_by_user(cls,search_term):
         return cls.objects.filter(user__username__icontains = search_term)
+
+    @classmethod
+    def update_profile_bio(cls, id,bio):
+        return cls.objects.filter(id = id).update(bio=bio)
+
