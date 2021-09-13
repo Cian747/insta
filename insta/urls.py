47d24857618b3acaf8b8d5105dd71db1bf6721e1
Cian <cian.omondi@student.moringaschool.com>
2021-09-13 22:57:40 +0300

    added proilfe urls and images

--- insta/urls.py
+++ insta/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('gram.urls')),
+    path('profile/',include('profiles.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
 
 ]
