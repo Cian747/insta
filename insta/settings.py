493e04d383a75f4493f1f9726c887f1694cf94ea
Cian <cian.omondi@student.moringaschool.com>
2021-09-14 11:10:52 +0300

    debug mode

--- insta/settings.py
+++ insta/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
